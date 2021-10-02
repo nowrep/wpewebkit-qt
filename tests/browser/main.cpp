17543c27a364f403e99042982be81380ef567e2e
David Rosca <nowrep@gmail.com>
2021-10-02 18:01:55 +0200

    Browser: Allow to set own import path

--- tests/browser/main.cpp
+++ tests/browser/main.cpp
@@ -6,7 +6,11 @@ int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
-    qputenv("QML2_IMPORT_PATH", qPrintable(QDir::cleanPath(app.applicationDirPath() + "/../../qml")));
+    QByteArray importPath = qgetenv("QML2_IMPORT_PATH");
+    if (!importPath.isEmpty())
+        importPath.append(":");
+    importPath.append(QDir::cleanPath(app.applicationDirPath() + "/../../qml").toLocal8Bit());
+    qputenv("QML2_IMPORT_PATH", importPath.constData());
 
     QQmlApplicationEngine engine;
     engine.load(QUrl("qrc:main.qml"));
